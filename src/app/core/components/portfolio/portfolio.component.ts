ad98996deb97a08534a400d34c8341c7d14ac2bd
shafiq0225 <shafiqahamed.be@gmail.com>
2024-09-26 14:57:39 +0530

    add about section

--- src/app/core/components/portfolio/portfolio.component.ts
+++ src/app/core/components/portfolio/portfolio.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { HeroComponent } from "../../../feature/hero/hero.component";
+import { AboutComponent } from "../../../feature/about/about.component";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, AboutComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
