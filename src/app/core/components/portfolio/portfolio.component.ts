d6cb608cf9a8c1cdee6fcec8db114d375a2db64b
shafiq0225 <shafiqahamed.be@gmail.com>
2024-09-27 16:16:29 +0530

    add work experience section

--- src/app/core/components/portfolio/portfolio.component.ts
+++ src/app/core/components/portfolio/portfolio.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { HeroComponent } from "../../../feature/hero/hero.component";
 import { AboutComponent } from "../../../feature/about/about.component";
+import { WorkExperienceComponent } from "../../../feature/work-experience/work-experience.component";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, AboutComponent],
+  imports: [HeaderComponent, HeroComponent, AboutComponent, WorkExperienceComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
