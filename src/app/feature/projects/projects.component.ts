d28193d03fbea3097639682e5749dc81e51ea113
shafiq0225 <shafiqahamed.be@gmail.com>
2024-12-30 11:55:13 +0530

    airbnb project added

--- src/app/feature/projects/projects.component.ts
+++ src/app/feature/projects/projects.component.ts
@@ -11,6 +11,16 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   public projects = [
     {
+      title: 'AirBNB Clone',
+      description:
+        'End to end project clone of Airbnb - vacation rental app using Angular, Rxjs, TypeScript and more',
+      techList: ['Angular', 'TypeScript', 'HTML', 'CSS'],
+      github: 'https://github.com/shafiq0225/AirBNB-clone',
+      screenshot:
+        'https://res.cloudinary.com/du63kkxhl/image/upload/v1681629704/logo_smny53.png',
+      live: 'https://airbnb-clone-frontend-8w55.onrender.com/',
+    },
+    {
       title: 'Netflix Home Page',
       description:
         'Clone of Netflix website built using Angular as a Front-end',
@@ -29,48 +39,7 @@ export class ProjectsComponent {
       screenshot:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR697xyMAHvYprLralN94basdHM0w3SJXu6zNFiArgyTVwC-VaN8DG-0O2Jzy3NfL8EJto&usqp=CAU',
       live: 'https://shafiq0225.github.io/clean-calculator/',
-    },
-    {
-      title: 'Credit card input page challenge',
-      description:
-        'Worked upon a credit card input page challenge from frontEndMaster. Built a responsive card input component.',
-      techList: ['Angular', 'TypeScript', 'HTML', 'CSS'],
-      github: 'https://github.com/shafiq0225',
-      screenshot:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT--HxPlwIymjO6xEwvilwdr41taAhEm_SipNM4HDV3sMSiS0RovP2pAUQleZpv2D-Oi14&usqp=CAU',
-      live: 'https://pravinmane1.github.io/login-card-challenge/',
-    },
-
-    {
-      title: 'Book Store App',
-      description:
-        'This is a online book store app implementation using firebase. That includes Google signIn, Phone Number verification, Cart, Books listing of different courses, wishlist, profile, order management.',
-      techList: ['Android', 'Java', 'Firebase'],
-      github: 'https://github.com/shafiq0225',
-      screenshot:
-        'https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/android-app-icon.png',
-      live: 'https://github.com/pravinmane1/Bookambo-Online-book-store',
-    },
-    {
-      title: 'Online Car Renting Android App',
-      description:
-        'A car renting application made using firebase. User can book and rent a car using this app. This app has google sign in, email sign in, Virtual wallet, Cars listing and admin dashboard to manage the cars listing and to manage bookings.',
-      techList: ['Android', 'Java', 'Firebase'],
-      github: 'https://github.com/shafiq0225',
-      screenshot:
-        'https://www.nevinainfotech.com/new-ni-images/services_pages_img/android-app-development-img.svg',
-      live: 'https://github.com/pravinmane1/Safar-Car-Renting-Online-App',
-    },
-    {
-      title: 'Attendance Android App',
-      description:
-        'Attendance app for teacher to track attendance of different classes by adding timetable and attendance of the student.',
-      techList: ['Android', 'Java', 'XML', 'Firebase'],
-      github: 'https://github.com/shafiq0225',
-      screenshot:
-        'https://www.radicallogix.com/assets/img/student-attendance-features.png',
-      live: 'https://github.com/pravinmane1/AttendanceApp/',
-    },
+    }   
   ];
 
   public visibleProjects = this.projects.slice(0, 3);
